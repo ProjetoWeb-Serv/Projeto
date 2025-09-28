3a7f05ac4df0c9fe020c1aebe763e386397af562
Leonardo <leo.bonfanti@hotmail.com>
2025-09-28 17:08:04 -0300

    feat: melhora no css

--- views/inserir_aluno.view.php
+++ views/inserir_aluno.view.php
@@ -1,9 +1,17 @@
-<h2>Inserir Novo Aluno</h2>
+<div class="aluno_title">
+    <h2>Inserir Novo Aluno</h2>
+</div>
 
 <form id="formulario_alunos" method="POST" action="/alunos/gravar">
-    <input type="text" name="nome_aluno" placeholder="Insira o nome do aluno">
-    <input type="text" name="data_nascimento" placeholder="Insira a data de nascimento">
-    <button type="submit">Inserir aluno</button>
+    <div class="aluno_nome">
+        <input type="text" name="nome_aluno" placeholder="Insira o nome do aluno">
+    </div>
+    <div class="aluno_nascimento">
+        <input type="text" name="data_nascimento" placeholder="Insira a data de nascimento">
+    </div>
+    <div class="button_aluno">
+        <button type="submit">Inserir aluno</button>
+    </div>
 </form>
 <div id="message">
     
--- views/inserir_curso.view.php
+++ views/inserir_curso.view.php
@@ -1,9 +1,19 @@
+<div class="curso_title">
     <h2>Inserir Novo Curso</h2>
-
+</div>
     <form id="formulario_cursos" method="POST" action="/cursos/gravar">
-        <input type="text" name="nome_curso" placeholder="Insira o nome do curso">
-        <input type="text" name="carga_horaria" placeholder="Insira a carga horária">
-        <button type="submit">Inserir curso</button>
+        <div class="curso_nome">
+            <input type="text" name="nome_curso" placeholder="Insira o nome do curso">
+
+        </div>
+        <div class="curso_carga_horaria">
+            <input type="text" name="carga_horaria" placeholder="Insira a carga horária">
+            
+        </div>
+        <div class="button_curso">
+            <button type="submit">Inserir curso</button>
+            
+        </div>
     </form>
     <div id="message">    
     </div>
