c3114178214653fd6f06b2328e10bb4075c2f095
Leonardo <leo.bonfanti@hotmail.com>
2025-09-28 19:35:22 -0300

    feat: refact no css

--- views/inserir_matricula.view.php
+++ views/inserir_matricula.view.php
@@ -1,23 +1,31 @@
-<h1>Inserir nova matrícula</h1>
+<div class="matricula_title">   
+    <h1>Inserir nova matrícula</h1>
+</div>
 
-<form id="formulario_matriculas" method="POST" action="/matriculas/gravar">
-    <p>Selecione o aluno</p>
-    <select id="aluno" name="nome_aluno">
-            <?php 
-            foreach($_SESSION['alunos'] as $aluno){
-                echo '<option value="'.$aluno['nome_aluno'].'">' . $aluno['nome_aluno'] . '</option>';
-            }
-            ?> 
-    </select>
-    <p>Selecione o curso</p>
-    <select id="curso" name="nome_curso">
-            <?php 
-            foreach($_SESSION['cursos'] as $curso){
-                echo '<option value="'.$curso['nome_curso'].'">' . $curso['nome_curso'] . '</option>';
-            }
-            ?> 
-    </select>
-    <div class="button_aluno">
-        <button type="submit">Inserir matrícula</button>
-    </div>
-</form>
+<div class="form_matricula">
+    <form id="formulario_matriculas" method="POST" action="/matriculas/gravar">
+        <div class="select_div">
+            <p class="select_aluno">Selecione o aluno</p>
+            <select id="aluno" name="nome_aluno">
+                    <?php 
+                    foreach($_SESSION['alunos'] as $aluno){
+                        echo '<option value="'.$aluno['nome_aluno'].'">' . $aluno['nome_aluno'] . '</option>';
+                    }
+                    ?> 
+            </select>
+        </div>
+        <div class="select_div">
+            <p class="select_curso">Selecione o curso</p>
+            <select id="curso" name="nome_curso">
+                    <?php 
+                    foreach($_SESSION['cursos'] as $curso){
+                        echo '<option value="'.$curso['nome_curso'].'">' . $curso['nome_curso'] . '</option>';
+                    }
+                    ?> 
+            </select>
+        </div>
+        <div class="button_aluno">
+            <button type="submit">Inserir matrícula</button>
+        </div>
+    </form>
+</div>
