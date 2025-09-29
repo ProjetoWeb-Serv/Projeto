6457661f21a29d6c19f1d57c3b189f7559add83d
Leonardo <leo.bonfanti@hotmail.com>
2025-09-29 10:37:49 -0300

    feat: refact na home page

--- views/home.view.php
+++ views/home.view.php
@@ -1,1 +1,11 @@
-<h1>Bem vindo a Dog Cursos</h1>
+<div class="title_home">
+    <h1>Bem vindo a Dog Cursos</h1>
+</div>
+<div class="subtitle_home">
+    <p>
+        Aqui você encontra os melhores cursos para você aprender e se desenvolver na sua carreira.
+    </p>
+</div>
+<div class="subtitle_home">
+    <p>Seu portal de cursos online</p>
+</div>
