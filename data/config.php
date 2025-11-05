cbb9bfa4faa5055057c3b1a018818f57150bdd36
João Altevir <j.altevirneto@gmail.com>
2025-11-05 00:08:17 -0300

    feat: add classe de conexão no banco

---
+++ data/config.php
@@ -0,0 +1,21 @@
+<?php
+
+Class Connection{
+    private static $db;
+    
+    public static function conectar(){
+
+        try {
+            $user = 'root';
+            $pass = '';
+            if(!isset(self::$db)){
+                self::$db = new PDO('mysql:host=localhost;dbname=web_serv', $user, $pass);
+            }
+            print("<h>conectado com sucesso</h>");
+            return self::$db;
+        } catch (\Throwable $th) {
+            die("Error de conexão: " . $th->getMessage());
+        }
+
+    } 
+}
